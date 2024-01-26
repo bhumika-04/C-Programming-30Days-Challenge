c1c10d5b99c33351eb48eb0ab9e357af67197cdb
Bhumika Shivhare <116136314+bhumika-04@users.noreply.github.com>
2024-01-26 21:06:02 +0530

    Create my_program.c

---
+++ Day2/my_program.c
@@ -0,0 +1,15 @@
+// This is a comment
+#include <stdio.h> // This is a preprocessor directive
+
+void greet(); // This is a function declaration
+
+int main() // This is the main function
+{
+    greet(); // This is a function call
+    return 0; // This is a return statement
+}
+
+void greet() // This is a user-defined function
+{
+    printf("Hello, world!\n"); // This is a library function
+}
